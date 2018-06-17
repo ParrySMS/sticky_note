6dd60bc16569effff05ebd28a5aba377c62d2050
ParrySMS <yh@szu.me>
2018-06-18 01:37:36 +0800

    move common

---
+++ stApp/common/PmCheck.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: haier
+ * Date: 2018-6-18
+ * Time: 1:20
+ */
+
+namespace stApp\common;
+
+
+class PmCheck
+{
+
+    //todo: 参数过滤作为 private 方法  参数检查作为 public
+}
