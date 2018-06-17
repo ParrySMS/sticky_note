8a77445b614e1b867bbccced435c2b0fdb8fc2b1
ParrySMS <yh@szu.me>
2018-06-17 19:09:58 +0800

    new file:   config/msg.php 	new file:   config/params.php 	new file:   config/wxApp.php 	new file:   route.php

---
+++ config/database_info.php
@@ -0,0 +1,11 @@
+<?php
+	define("DATABASE_TYPE","mysql");
+	define("DATABASE_NAME", "");
+	define("SERVER", "");
+	define("USERNAME", "");
+	define("PASSWORD", "");
+	define("CHARSET", "utf8");
+	define("PORT", "3306");
+    define("CHECK_INTERVAL",300);
+
+?>
---
+++ config/msg.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: haier
+ * Date: 2018-1-18
+ * Time: 12:14
+ */
+define('CONTACT_WX','');
+
+
