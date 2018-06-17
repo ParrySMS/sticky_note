a9c0e53efccbccb1f256567f8b450fd347f84634
ParrySMS <yh@szu.me>
2018-06-17 22:07:38 +0800

    new file:   stApp/service/WxRequest.php

--- config/wxApp.php
+++ config/wxApp.php
@@ -2,3 +2,7 @@
 
 //define("APPID","");
 //define("APPSECRET","");
+
+//BG
+define("APPID","wxde6a9e2fd63767d5");
+define("APPSECRET","");
---
+++ stApp/service/WxRequest.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: haier
+ * Date: 2018-6-17
+ * Time: 21:30
+ */
+namespace stApp\service;
+
+class WxRequest
+{
+
+    private $app_id = APPID;
+    private $app_secret = APPSECRET;
+
+    /**
+     * WxRequest constructor.
+     */
+    public function __construct()
+    {
+    }
+
+
+
+}
